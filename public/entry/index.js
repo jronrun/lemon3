6571edb244366fff4d04f159ead6bfbd3efccc77
yejiwei <yejiwei@u51.com>
2016-02-15 16:22:04 +0800

    CodeMirror

--- public/entry/index.js
+++ public/entry/index.js
@@ -2,6 +2,7 @@ require('../css/style.styl');
 
 //Bootstrap tooltips require Tether (http://github.hubspot.com/tether/)
 Tether = require('tether');
+CodeMirror = require('codemirror/lib/codemirror.js');
 
 $(function() {
   $('#abc').text(VERSION+"hellw");

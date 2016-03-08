8faf65f904881975d2c4df14ba12f87771c11777
yejiwei <yejiwei@u51.com>
2016-03-08 20:28:51 +0800

    modular admin template

--- public/entry/index.js
+++ public/entry/index.js
@@ -5,4 +5,20 @@ Tether = require('tether');
 lemon = require('lemon/coffee/lemon.coffee');
 lemon.register(require('lz-string'));
 
-//require('font-awesome/css/font-awesome.css');
+require('font-awesome/css/font-awesome.css');
+
+$(function () {
+
+  var themeSettings = (localStorage.getItem('themeSettings')) ? JSON.parse(localStorage.getItem('themeSettings')) :
+  {};
+  var themeName = themeSettings.themeName || '';
+  if (themeName)
+  {
+    require('../css/app-' + themeName + '.scss');
+  }
+  else
+  {
+    require('../css/app.scss');
+  }
+
+});

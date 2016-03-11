088dd6d5a27af69ccc28d2104895dce139246b53
yejiwei <yejiwei@u51.com>
2016-03-11 17:53:15 +0800

    handlebars comparisons

--- app/coms/helpers/hbsregist.js
+++ app/coms/helpers/hbsregist.js
@@ -1,9 +1,11 @@
 'use strict';
 
-var layouts = require('handlebars-layouts');
+var layouts = require('handlebars-layouts'),
+  compare = require('./comparisons');
 
 module.exports.register = function (handlebars, options) {
   options = options || {};
 
   handlebars.registerHelper(layouts(handlebars));
+  compare.register(handlebars);
 };

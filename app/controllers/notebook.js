3d56ebd428abe6b27c81eb64c61af508d3292493
yejiwei <yejiwei@u51.com>
2016-05-24 17:16:04 +0800

    notebook

--- app/controllers/notebook.js
+++ app/controllers/notebook.js
@@ -10,45 +10,9 @@ module.exports = function (app) {
   app.use(index.action, router);
 };
 
+/**
+ * Notebook home
+ */
 router.get(index.do, function (req, res, next) {
-
-  async.waterfall([
-    function(callback) {
-      callback(null, 'one', 'two');
-    },
-    function(arg1, arg2, callback) {
-      // arg1 now equals 'one' and arg2 now equals 'two'
-      callback(null, 'three');
-    },
-    function(arg1, callback) {
-      // arg1 now equals 'three'
-      setTimeout(function () {
-        callback(null, 'done');
-      }, 3000);
-
-    },
-    function(arg1, callback) {
-      note.nextId(function (id) {
-        callback(null, arg1 + ' ' + id);
-      });
-    }
-  ], function (err, result) {
-    // result now equals 'done'
-    log.info('result: ' +result);
-    return result;
-  });
-
-  note.findById('56cd685a795aa57d37df8613', function(err, doc) {
-    log.info(doc);
-  });
-
-  note.nextId().then(function (id) {
-    log.info('nextId: ' + id);
-  });
-
-  note.nextId(function (id) {
-    log.info('nextId: ' + id);
-  });
-
   res.render(index);
 });

de00f8b7e23753121784f3966d9e5966f3cf682c
yejiwei <yejiwei@u51.com>
2016-07-28 11:12:23 +0800

    mapi

--- public/entry/mapi.js
+++ public/entry/mapi.js
@@ -3,9 +3,33 @@
  */
 var mapis = {
 
-  newapi: function() {
-    return lemon.previews((location.origin || '') + '/api', function(view, preview) {
+  instances: {},
+  instance: {
+    defaultId: 'default',
 
+    autoId: function() {
+      return ('instance' + lemon.uniqueId());
+    },
+
+    add: function(instanceId, view, preview) {
+      instanceId || mapis.instance.autoId();
+      view.apiInstanceId = instanceId;
+      preview.apiInstanceId = instanceId;
+
+      mapis.instances[instanceId] = {
+        view: view,
+        preview: preview
+      };
+    },
+
+    gets: function(instanceId) {
+      return mapis.instances[instanceId] || null;
+    }
+  },
+
+  newapi: function(instanceId) {
+    return lemon.previews((location.origin || '') + '/api', function(view, preview) {
+      mapis.instance.add(instanceId, view, preview);
     }, false, function(view) {
       $('body', view.getDocument()).css({
         'padding-top': '4.15rem'
@@ -14,7 +38,7 @@ var mapis = {
   },
 
   initialize: function() {
-    mapis.newapi();
+    mapis.newapi(mapis.instance.defaultId);
   }
 };
 

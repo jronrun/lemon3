e09c56c865e12e57f9b7a8d07176b3df2cc82666
yejiwei <yejiwei@u51.com>
2016-07-28 19:20:00 +0800

    mapi layout

--- public/entry/mapi.js
+++ public/entry/mapi.js
@@ -1,6 +1,16 @@
 /**
  *
  */
+
+function $$(selector, instanceId) {
+  var aInstance = mapis.instance.gets(instanceId || mapis.instance.defaultId);
+  if (!aInstance || !aInstance.view) {
+    return [];
+  }
+
+  return $(selector, aInstance.view.getDocument())
+}
+
 var mapis = {
 
   instances: {},
@@ -22,23 +32,72 @@ var mapis = {
       };
     },
 
+    getDefault: function() {
+      return mapis.instance.gets(mapis.instance.defaultId);
+    },
     gets: function(instanceId) {
       return mapis.instances[instanceId] || null;
     }
   },
 
-  newapi: function(instanceId) {
+  tools: null,
+  tool: {
+    initialize: function() {
+      mapis.tools = lemon.popover('#mapi_tool', {
+        arrow: false,
+        trigger: 'manual',
+        content: function() {
+          return lemon.tmpl($('#mapi_tool_tmpl').html(), {});
+        }
+      }, {
+        inserted: function(el) {
+          $(el).hide();
+        },
+        shown: function(el) {
+          var w = Math.floor($(window).width() / 2);
+          $(el).css({
+            width: w,
+            'max-width': w,
+            border: 0,
+            top: -11,
+            left: w + 110,
+            'z-index': 50001
+          });
+
+          $(el).find('.popover-content').css({
+            'padding': 0
+          });
+
+          $(el).slideDown();
+        }
+      });
+
+      mapis.tools.show();
+    }
+  },
+
+  newapi: function(instanceId, domReadyCallback) {
     return lemon.previews((location.origin || '') + '/api', function(view, preview) {
       mapis.instance.add(instanceId, view, preview);
-    }, false, function(view) {
+    }, false, function(view, preview) {
       $('body', view.getDocument()).css({
         'padding-top': '4.15rem'
       });
+
+      lemon.isFunc(domReadyCallback) && domReadyCallback(view, preview);
     });
   },
 
   initialize: function() {
-    mapis.newapi(mapis.instance.defaultId);
+    mapis.newapi(mapis.instance.defaultId, function() {
+      if (lemon.isMediumUpView()) {
+        mapis.tool.initialize();
+      }
+    });
+
+    //TODO remove
+    global.mapis = mapis;
+    global.$$ = $$;
   }
 };
 

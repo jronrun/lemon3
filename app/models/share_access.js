b7a400dcdeb5b1f3da0d32208e56140daf49b6a6
yejiwei <yejiwei@u51.com>
2016-08-15 10:53:32 +0800

    share access model

---
+++ app/models/share_access.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var log = log_from('share_access');
+
+var model = schema({
+  id: { type: 'integer', required: true },
+  share: { type: 'integer', required: true },
+  history: { type: 'integer' },
+
+  type: { type: 'integer', enum: [1, 2], required: true, const: { 1: 'Open', 2: 'Execute'} },
+  create_by: {
+    type: 'object',
+    properties: {
+      id: { type: 'string', allowEmpty: false },
+      name: { type: 'string', required: true },
+      ip: { type: 'string', allowEmpty: false }
+    }
+  },
+  last_modify_time: { type: 'date', required: true },
+  create_time: { type: 'date', required: true }
+});
+
+var shareAccess = model_bind('share_access', model);
+
+shareAccess.newAccess = function(access, resultCall, requestInfo) {
+  async.waterfall([
+    function(callback) {
+      var target = _.extend({
+        type: 1,
+        create_by: {
+          id: requestInfo.usr.id,
+          name: requestInfo.usr.name,
+          ip: requestInfo.clientIP
+        },
+        last_modify_time: new Date(),
+        create_time: new Date()
+      }, access || {});
+
+      if (2 == target.type && !target.history) {
+        return resultCall(answer.fail('execute access must provide history id'));
+      }
+
+      callback(null, target);
+    },
+
+    function(target, callback) {
+      share.nextId(function (id) {
+        target.id = id;
+
+        var check = shareAccess.validate(target);
+        if (!check.valid) {
+          return resultCall(answer.fail(check.msg));
+        }
+
+        share.insertOne(target, function(err, result) {
+          if (err) {
+            return resultCall(answer.fail(err.message));
+          }
+
+          if (1 != result.insertedCount) {
+            return resultCall(answer.fail('create fail'));
+          }
+
+          target._id = result.insertedId.toString();
+          callback(null, target);
+        });
+      });
+    }
+  ], function(err, result) {
+    resultCall(answer.succ(result));
+  });
+};
+
+module.exports = shareAccess;
+

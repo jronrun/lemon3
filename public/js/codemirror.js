84208d78fbb85171999eae28d74074d854363fb4
yejiwei <yejiwei@u51.com>
2016-09-01 10:44:05 +0800

    note menu theme

--- public/js/codemirror.js
+++ public/js/codemirror.js
@@ -71,7 +71,7 @@ lemon.fmtjson = function(target) {
 };
 
 var languages = {}, modes = {}, loadedTheme = ['default', 'lemon'], themes = [
-  'default','3024-day','3024-night','abcdef','ambiance','base16-dark','base16-light','bespin','blackboard','cobalt',
+  'default','lemon','3024-day','3024-night','abcdef','ambiance','base16-dark','base16-light','bespin','blackboard','cobalt',
   'colorforth','dracula','eclipse','elegant','erlang-dark','hopscotch','icecoder','isotope','lesser-dark','liquibyte',
   'material','mbo','mdn-like','midnight','monokai','neat','neo','night','paraiso-dark','paraiso-light','pastel-on-dark',
   'railscasts','rubyblue','seti','solarized,dark','solarized,light','the-matrix','tomorrow-night-bright','tomorrow-night-eighties',

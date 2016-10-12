2200ff620277f5710e58bca0dd663768536740d4
yejiwei <yejiwei@u51.com>
2016-10-12 19:54:14 +0800

    upgrade to bootstrap#v4.0.0-alpha.4

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -81,7 +81,19 @@ module.exports = {
       $: 'jquery',
       jQuery: 'jquery',
       "window.jQuery": 'jquery',
-      "window.Tether": 'tether'
+      Tether: "tether",
+      "window.Tether": "tether",
+      Tooltip: "exports?Tooltip!bootstrap/js/dist/tooltip",
+      Alert: "exports?Alert!bootstrap/js/dist/alert",
+      Button: "exports?Button!bootstrap/js/dist/button",
+      Carousel: "exports?Carousel!bootstrap/js/dist/carousel",
+      Collapse: "exports?Collapse!bootstrap/js/dist/collapse",
+      Dropdown: "exports?Dropdown!bootstrap/js/dist/dropdown",
+      Modal: "exports?Modal!bootstrap/js/dist/modal",
+      Popover: "exports?Popover!bootstrap/js/dist/popover",
+      Scrollspy: "exports?Scrollspy!bootstrap/js/dist/scrollspy",
+      Tab: "exports?Tab!bootstrap/js/dist/tab",
+      Util: "exports?Util!bootstrap/js/dist/util",
     }),
     new webpack.DefinePlugin({
       VERSION: JSON.stringify("5fa3b9")

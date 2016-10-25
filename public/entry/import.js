afc5c7218d034aa7ec0367fee62bb408234818ce
yejiwei <yejiwei@u51.com>
2016-10-25 17:34:50 +0800

    import start

--- public/entry/import.js
+++ public/entry/import.js
@@ -10,10 +10,54 @@ function leave() {
 var imp = {
   id: '#import_container',
 
+  views: function (text, hiddenCall, noneWrapJSON, domReadyCall) {
+    var jsonOptions = false;
+    if (!noneWrapJSON) {
+      if (mirror.isJson(text, true)) {
+        text = lemon.fmtjson(text);
+        jsonOptions = {
+          mirror: mirror,
+          isDecode: true
+        };
+      }
+    }
+
+    //lemon.preview(text, callback, jsonOptions, domReadyCallbackIfUrl, modalOptions, modalEvents)
+    return lemon.preview(text, function () {
+    }, jsonOptions, domReadyCall, false, {
+      hidden: function () {
+        lemon.isFunc(hiddenCall) && hiddenCall();
+      }
+    });
+  },
+
   start: {
+    id: '#start_source',
     intl: function () {
       lemon.tmpls('#imp_start_tmpl', {}, imp.id);
-    }
+
+      $('button[data-start]').click(function () {
+        switch (lemon.data(this, 'start')) {
+          case 1:
+            alert('swagger');
+            break;
+          case 2:
+            alert('curl');
+            break;
+          case 3:
+            imp.views($(imp.start.id).val());
+            break;
+        }
+      });
+    },
+  },
+
+  swagger: {
+
+  },
+
+  curl: {
+
   },
 
   initialize: function () {

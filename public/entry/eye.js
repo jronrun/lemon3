c2312c9bee1c43761d43236274eeddd6ea0fa188
yejiwei <yejiwei@u51.com>
2017-02-05 21:07:27 +0800

    golden.js

--- public/entry/eye.js
+++ public/entry/eye.js
@@ -1,14 +1,70 @@
 /**
  *
  */
+var gl = require('../js/golden');
+
+function leave() {
+  eye.noteEvt('LEAVE');
+}
 
 var eye = {
+  instance: null,
+
+  layout: {
+    intl: function () {
+      eye.instance = gl({
+        content: [{
+          type: 'row',
+          content: [
+            gl.kits('eyesv', {
+              id: 'eyes_note',
+              src: lemon.fullUrl('/note')
+            }),
+            gl.kits('eyesv', {
+              id: 'eyes_preview',
+              src: lemon.fullUrl('/show')
+            })
+          ]
+        }]
+      }).register( 'eyesv', function(container, state){
+        container.on('resize', function(){
+          container.lframe.attr({
+            height: container.height,
+            width: container.width
+          });
+        });
+
+        var eType = lemon.ltrim(state.id, 'eyes_');
+        container.lframe = lemon.iframe({
+          id: state.id, name: state.id,
+          frameborder: 0,
+          src: state.src
+        }, container.getElement());
+
+        eye[eType] = container;
+      }).init(function () {
+        lemon.delay(function () {
+          eye.noteEvt('SNAPLOAD');
+        }, 800);
+      }).contentStyle({
+        border: 'none'
+      });
+    }
+  },
+
+  noteEvt: function (evtN, evtData, ackCallback) {
+    if (eye.note && eye.note.lframe) {
+      eye.note.lframe.tellEvent(evtN, evtData, ackCallback);
+    }
+  },
 
   initialize: function() {
+    lemon.homeProgress();
     if (lemon.isMediumUpView()) {
       lemon.console();
     }
 
+    eye.layout.intl();
     lemon.subMsg(function (data) {
       // lemon.info(data, 'Eye received msg');
       if (data && data.event) {
@@ -19,7 +75,20 @@ var eye = {
         }
       }
     });
+
+    lemon.unload(function () {
+      leave();
+    });
+
+    lemon.delay(function () {
+      lemon.homeProgressEnd();
+    }, 900);
   }
 };
 
 $(function () { eye.initialize(); });
+
+//TODO rem
+global.eye=eye;
+global.gl=gl;
+global.$=$;
---
+++ public/js/golden.js
@@ -0,0 +1,86 @@
+/**
+ *
+ */
+// var golden = require('golden-layout');
+require('golden-layout/src/css/goldenlayout-base.css');
+require('golden-layout/src/css/goldenlayout-light-theme.css');
+var GoldenLayout = require('imports?window=>global!golden-layout/dist/goldenlayout');
+
+var helper = function (inst, options) {
+  options = options || {};
+  var tools = {
+    target: inst,
+
+    contentStyle: function (targetCss) {
+      $('.lm_content').css(targetCss || {});
+      return tools;
+    },
+
+    register: function (name, component) {
+      inst.registerComponent(name, component);
+      return tools;
+    },
+
+    onLayout: function (evtName, evtCall) {
+      if (lemon.isFunc(evtCall)) {
+        inst.on(evtName, evtCall);
+      }
+      return tools;
+    },
+
+    init: function (initializedCall) {
+      if (lemon.isFunc(initializedCall)) {
+        tools.onLayout('initialised', function () {
+          return initializedCall(inst);
+        });
+      }
+      inst.init();
+      return tools;
+    },
+
+    rowColumnTgl: function(beforeCall, afterCall) {
+      lemon.isFunc(beforeCall) && beforeCall();
+      var oldEl = inst.root.contentItems[ 0 ],
+        newEl = inst.createContentItem({
+          type: oldEl.isRow ? 'column' : 'row',
+          content: []
+        }), i;
+
+      //Prevent it from re-initialising any child items
+      newEl.isInitialised = true;
+
+      for( i = 0; i < oldEl.contentItems.length; i++ ) {
+        newEl.addChild( oldEl.contentItems[ i ] );
+      }
+
+      inst.root.replaceChild( oldEl, newEl );
+      lemon.isFunc(afterCall) && afterCall();
+    }
+  };
+
+  return tools;
+};
+
+var gl = function (config, options) {
+  options = lemon.extend({
+  }, options || {});
+  config = config || {};
+
+  config.settings = lemon.extend({
+    hasHeaders: false
+  }, config.settings || {});
+
+  var inst = new GoldenLayout(config);
+
+  return helper(inst, options);
+};
+
+gl.kits = function (componentName, componentState, type, options) {
+  return lemon.extend({
+    type: type || 'component',  //Can be row, column, stack, component or root
+    componentName: componentName,
+    componentState: componentState || {}
+  }, options || {});
+};
+
+module.exports = gl;
